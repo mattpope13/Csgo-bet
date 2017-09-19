4fd1bfff3a3598752203c2fc10101bc9183f9688
mattpope13 <32112229+mattpope13@users.noreply.github.com>
2017-09-19 22:24:34 +0100

    Add files via upload

---
+++ csgojackpotV2-master/steamauth/settings.php
@@ -0,0 +1,6 @@
+<?php
+$steamauth['apikey'] = "apikey"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "localhost"; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "/"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+?>
